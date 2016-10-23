5669bc2b1defce9e49d6e127df9e055278eff961
Albin Sunnanbo <albin@sunnanbo.com>
2016-10-23 13:09:05 +0200

    Model validating OK with built in validation

--- FluentValidation_Issue_349_IEnumerableModels/src/FluentValidation_Issue_349_IEnumerableModels/Controllers/HomeController.cs
+++ FluentValidation_Issue_349_IEnumerableModels/src/FluentValidation_Issue_349_IEnumerableModels/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using FluentValidation_Issue_349_IEnumerableModels.Models;
 
 namespace FluentValidation_Issue_349_IEnumerableModels.Controllers
 {
@@ -12,7 +13,44 @@ namespace FluentValidation_Issue_349_IEnumerableModels.Controllers
         {
             return View();
         }
-      
+
+        public IActionResult SingleModel()
+        {
+            var model = new MyModel
+            {
+                MyProperty = "Hello",
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult SingleModel(MyModel model)
+        {
+            return View(model);
+        }
+
+        public IActionResult IEnumerableModel()
+        {
+            var model = new List<MyModel>
+            {
+                new MyModel
+                {
+                    MyProperty = "Hello",
+                },
+                new MyModel
+                {
+                    MyProperty = "World",
+                },
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult IEnumerableModel(IEnumerable<MyModel> model)
+        {
+            return View(model);
+        }
+
         public IActionResult Error()
         {
             return View();
---
+++ FluentValidation_Issue_349_IEnumerableModels/src/FluentValidation_Issue_349_IEnumerableModels/Models/MyModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FluentValidation_Issue_349_IEnumerableModels.Models
+{
+    public class MyModel
+    {
+        [MinLength(100)]
+        public string MyProperty { get; set; }
+    }
+}
